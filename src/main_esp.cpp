e8c5a6de22feae67fdc5deab5c01936148c0b8cb
Oliver Westermann <owestermann@googlemail.com>
2024-08-02 19:07:44 +0200

    Implement state machine basics

--- src/main_esp.cpp
+++ src/main_esp.cpp
@@ -1,7 +1,7 @@
 #include <Arduino_GFX_Library.h>
-#include <Arduino_GFX_Library.h>
 #include <SoftwareSerial.h>
 #include <SPI.h>
+#include <time.h>
 
 #include "decoder.hpp"
 #include "example_data.hpp"
@@ -16,7 +16,7 @@ SoftwareSerial mySerial(RX_LELIT, -1, true);
 
 char *example_data_ptr = (char *)example_data;
 Decoder decoder;
-int i;
+StateMachine *state_machine = NULL;
 
 void setup()
 {
@@ -25,7 +25,6 @@ void setup()
     Serial.println("Hello World!");
 
     pinMode(RX_LELIT, INPUT);
-
     mySerial.begin(9600);
     if (!mySerial)
     { // If the object did not initialize, then its configuration is invalid
@@ -33,35 +32,16 @@ void setup()
     }
 
     gfx->begin();
-    gfx->fillScreen(WHITE);
-    gfx->setCursor(100, 100);
-    gfx->setTextColor(BLACK, WHITE);
-    gfx->setTextSize(2);
+    gfx->fillScreen(BLACK);
+    gfx->setTextColor(WHITE, BLACK);
     gfx->setTextWrap(false);
-}
 
-void general_data_display(lelit_data_t *data)
-{
-    gfx->setCursor(0, 40);
-    gfx->printf("   Mode: %6s\n", data->mode == COFFEE ? "Coffee" : "Steam");
-    gfx->printf("   Steam:   %3d", data->steam_temp);
-    // Use special functionality to write degree symbol
-    gfx->write(0xF8);
-    gfx->printf("C\n");
-    gfx->printf("   Target:  %3d", data->target_steam_temp);
-    gfx->write(0xF8);
-    gfx->printf("C\n");
-    gfx->printf("   Coffee:  %3d", data->coffee_temp);
-    gfx->write(0xF8);
-    gfx->printf("C\n");
-    gfx->printf("   Boost:   %3ds\n", data->boost_countdown);
-    gfx->printf("   Heating: %3d\n", data->heating_element_on);
-    gfx->printf("   Pump:    %3d\n", data->pump_on);
+    state_machine = new StateMachine(gfx);
 }
 
 void loop()
 {
-    // we do not have serial data yet, so iterate over example data
+    // we do not known if have serial data yet, so iterate over example data
     bool real_data = false;
     bool updated = false;
     char rc = '\0';
@@ -89,15 +69,13 @@ void loop()
 
     if (updated)
     {
-        i++;
-        general_data_display(&decoder.data);
+        state_machine->update_state_machine(&decoder.data);
+        state_machine->draw_state(40, 40, false);
+
         if (!real_data)
         {
             // delay a bit to simulate real data
-            delay(200);
+            delay(100);
         }
     }
-
-    gfx->setCursor(100, 200);
-    gfx->println(i);
 }
--- src/state_machine.cpp
+++ src/state_machine.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 #include "state_machine.hpp"
 
@@ -9,68 +11,157 @@
 #define INFO false
 #define info(...) if(INFO) { printf(__VA_ARGS__); } else {};
 
-StateMachine::StateMachine() {
+
+
+long get_time()
+{
+    struct timespec tp;
+    clock_gettime(CLOCK_MONOTONIC, &tp);
+    return tp.tv_sec * 1000 + tp.tv_nsec / 1000000;
+}
+
+StateMachine::StateMachine(Arduino_GFX *gfx_ptr)
+{
     state = StateMachine::State::IDLE;
+    if (gfx_ptr == NULL)
+    {
+        debug("Error: gfx_ptr is NULL\n");
+        return;
+    }
+    gfx = gfx_ptr;
     debug("State machine initialized\n");
 };
 
-void StateMachine::update_state_machine(lelit_data_t *data)
+void StateMachine::change_state(State new_state)
+{
+    previous_state = state;
+    state = new_state;
+    last_state_change_ms = get_time();
+    debug("State changed from %s to %s at %d\n", get_state_string(previous_state), get_state_string(), last_state_change_ms);
+    debug("Clear display\n");
+    gfx->fillScreen(BLACK);
+};
+
+void StateMachine::update_state_machine(lelit_data_t *new_data)
 {
     info("Updating state machine, current state: %s\n", get_state_string());
-    StateMachine::State old_state = state;
+    memcpy(&last_data, new_data, sizeof(lelit_data_t));
     switch (state)
     {
     case StateMachine::State::IDLE:
-        if (data->pump_on)
+        if (last_data.pump_on)
         {
-            state = StateMachine::State::BREWING;
+            change_state(StateMachine::State::BREWING);
         }
-        else if (data->target_steam_temp < 100)
+        else if (last_data.target_steam_temp < 100)
         {
-            state = StateMachine::State::ERROR;
-        } else if (
-            (abs(data->target_steam_temp - data->steam_temp) > heating_threshold) || 
-            (abs(data->coffee_temp - coffee_temp_target) > heating_threshold)
-            )
+            change_state(StateMachine::State::ERROR);
+        }
+        else if (
+            (abs(last_data.target_steam_temp - last_data.steam_temp) > heating_threshold) ||
+            (abs(last_data.coffee_temp - coffee_temp_target) > heating_threshold))
         {
-            state = StateMachine::State::HEATING;
+            change_state(StateMachine::State::HEATING);
         }
         break;
     case StateMachine::State::HEATING:
-        if (data->pump_on)
+        if (last_data.pump_on)
         {
-            state = StateMachine::State::BREWING;
+            change_state(StateMachine::State::BREWING);
         }
-        else if (data->target_steam_temp < 100)
+        else if (last_data.target_steam_temp < 100)
         {
-            state = StateMachine::State::ERROR;
-        } else if (data->heating_element_on == false)
+            change_state(StateMachine::State::ERROR);
+        }
+        else if (last_data.heating_element_on == false)
         {
-            state = StateMachine::State::IDLE;
+            change_state(StateMachine::State::IDLE);
         }
         break;
     case StateMachine::State::ERROR:
-        if (data->target_steam_temp > 100)
+        if (last_data.target_steam_temp > 100)
         {
-            state = StateMachine::State::IDLE;
+            change_state(StateMachine::State::IDLE);
         }
         break;
     case StateMachine::State::BREWING:
-        if (data->pump_on == false)
+        if (last_data.pump_on == false)
         {
-            state = StateMachine::State::IDLE;
+            // Store brewing time to display in idle for a few more seconds
+            last_brewing_time_ms = get_time_in_current_state();
+            change_state(StateMachine::State::IDLE);
         }
         break;
     default:
         break;
     }
-    if (old_state != state)
+};
+
+void StateMachine::draw_state(int x, int y, bool debug)
+{
+    int time_brewing = get_time_in_current_state();
+
+    switch (state)
     {
-        debug("State changed from %s to %s\n", get_state_string(old_state), get_state_string());
+    case StateMachine::State::BREWING:
+        time_brewing = get_time_in_current_state();
+        draw_state_brewing(x, y, time_brewing, debug);
+        break;
+    case StateMachine::State::IDLE:
+        if (previous_state == StateMachine::State::BREWING && get_time_in_current_state() < 5000)
+        {
+            draw_state_brewing(x, y, last_brewing_time_ms, debug);
+            draw_state_default(x, y + 100, debug);
+            break;
+        }
+        else
+        {
+            // intentional fallthrough
+            ;
+        }
+    default:
+        draw_state_default(x, y, debug);
+        break;
     }
 };
 
-const char* StateMachine::get_state_string(StateMachine::State state) {
+void StateMachine::draw_state_default(int x, int y, bool debug)
+{
+    gfx->setTextSize(1);
+    gfx->setCursor(x, y);
+    gfx->setTextColor(WHITE, BLACK);
+
+    gfx->printf("   Mode: %6s\n", last_data.mode == COFFEE ? "Coffee" : "Steam");
+    gfx->printf("   Steam:   %3d/%3d", last_data.steam_temp, last_data.target_steam_temp);
+    // Use special functionality to write degree symbol
+    gfx->write(0xF8);
+    gfx->printf("C\n");
+    gfx->printf("   Coffee:  %3d", last_data.coffee_temp);
+    gfx->write(0xF8);
+    gfx->printf("C\n");
+    gfx->printf("   Boost:   %3ds\n", last_data.boost_countdown);
+    gfx->printf("   Heating: %3d\n", last_data.heating_element_on);
+    gfx->printf("   Pump:    %3d\n", last_data.pump_on);
+
+    gfx->printf("   State: %s\n", get_state_string());
+    gfx->printf("   Time in state: %6d\n", get_time_in_current_state());
+}
+
+void StateMachine::draw_state_brewing(int x, int y, int time_brewing, bool debug)
+{
+    gfx->setTextSize(3);
+    gfx->setCursor(x, y);
+
+    gfx->setTextColor(WHITE, BLACK);
+    gfx->printf("Brewing since\n");
+    gfx->setCursor(100, 100);
+    gfx->setTextColor(RED, BLACK);
+    gfx->setTextSize(5);
+    gfx->printf("%2.1fs", time_brewing / 1000.0);
+}
+
+const char *StateMachine::get_state_string(StateMachine::State state)
+{
     switch (state)
     {
     case StateMachine::State::IDLE:
@@ -89,4 +180,9 @@ const char* StateMachine::get_state_string(StateMachine::State state) {
 const char *StateMachine::get_state_string()
 {
     return get_state_string(state);
+};
+
+int StateMachine::get_time_in_current_state()
+{
+    return get_time() - last_state_change_ms;
 };
